411abceb4d67cbc9e81ec0c83acc0dd522871213
Kamil Podlasek <kamil.podlasek@gmail.com>
2022-10-19 20:36:01 +0200

    Add a flatten function with tests

--- flatten.test.ts
+++ flatten.test.ts
@@ -1,7 +1,74 @@
 import { flatten } from "./flatten";
 
-describe("flatten", () => {
-  test("works correctly", () => {
-    expect(flatten()).toBe(null);
+describe("flatten works correctly when the input is", () => {
+  describe("not an array and is", () => {
+    test("null", () => {
+      expect(() => flatten(null)).toThrow(new Error("Input is not an array!"));
+    });
+
+    test("undefined", () => {
+      expect(() => flatten(undefined)).toThrow(
+        new Error("Input is not an array!")
+      );
+    });
+
+    test("a number", () => {
+      expect(() => flatten(1)).toThrow(new Error("Input is not an array!"));
+    });
+
+    test("a string", () => {
+      expect(() => flatten("a")).toThrow(new Error("Input is not an array!"));
+    });
+
+    test("an object", () => {
+      expect(() => flatten({})).toThrow(new Error("Input is not an array!"));
+    });
+
+    test("a set", () => {
+      expect(() => flatten(new Set())).toThrow(
+        new Error("Input is not an array!")
+      );
+    });
+  });
+
+  describe("an array", () => {
+    test("of numbers", () => {
+      expect(flatten([1, [2, [3]], 4])).toStrictEqual([1, 2, 3, 4]);
+    });
+
+    test("of primitives", () => {
+      expect(flatten([1, ["a", [false]], 4])).toStrictEqual([1, "a", false, 4]);
+    });
+
+    test("of primitives and objects", () => {
+      expect(flatten([1, ["a", [false], { a: "b" }], 4])).toStrictEqual([
+        1,
+        "a",
+        false,
+        { a: "b" },
+        4,
+      ]);
+    });
+
+    test("of primitives and empty arrays", () => {
+      expect(flatten([1, ["a", [], [false]], [], 4, []])).toStrictEqual([
+        1,
+        "a",
+        false,
+        4,
+      ]);
+    });
+  });
+
+  test("an empty array", () => {
+    expect(flatten([])).toStrictEqual([]);
+  });
+
+  test("a very nested array", () => {
+    expect(
+      flatten([
+        [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[1]]]]]]], 2]]]]]]], 3]]]]]]]]]]]]]]]],
+      ])
+    ).toStrictEqual([1, 2, 3]);
   });
 });
--- flatten.ts
+++ flatten.ts
@@ -1,1 +1,17 @@
-export const flatten = () => null;
+type ValueOrArray<T> = T | ValueOrArray<T>[];
+type NestedValuesArray = ValueOrArray<any>;
+
+const flattenArray = (value: NestedValuesArray) =>
+  value.reduce(
+    (acc, el) =>
+      Array.isArray(el) ? [...acc, ...flattenArray(el)] : [...acc, el],
+    []
+  );
+
+export const flatten = (
+  value: NestedValuesArray | any
+): NestedValuesArray | Error => {
+  if (!Array.isArray(value)) throw Error("Input is not an array!");
+
+  return flattenArray(value);
+};
